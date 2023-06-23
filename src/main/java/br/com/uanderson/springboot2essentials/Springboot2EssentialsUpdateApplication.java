1b84a49de35e0498af947b5755c8bd8408189776
Uanderson Ferreira de Oliveira <uanderson.oliveira@estudante.ifto.edu.br>
2023-06-23 17:28:06 -0300

    Projeto inicial

---
+++ src/main/java/br/com/uanderson/springboot2essentials/Springboot2EssentialsUpdateApplication.java
@@ -0,0 +1,13 @@
+package br.com.uanderson.springboot2essentials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot2EssentialsUpdateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot2EssentialsUpdateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/uanderson/springboot2essentials/Springboot2EssentialsUpdateApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.uanderson.springboot2essentials;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot2EssentialsUpdateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
