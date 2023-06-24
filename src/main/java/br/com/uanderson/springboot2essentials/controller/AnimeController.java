f5ad4469573ed98f5fdf21e11a30a54b9aa56864
Uanderson Ferreira de Oliveira <uanderson.oliveira@estudante.ifto.edu.br>
2023-06-24 16:54:19 -0300

    Método GET part-02

--- src/main/java/br/com/uanderson/springboot2essentials/controller/AnimeController.java
+++ src/main/java/br/com/uanderson/springboot2essentials/controller/AnimeController.java
@@ -5,7 +5,11 @@ import br.com.uanderson.springboot2essentials.service.AnimeService;
 import br.com.uanderson.springboot2essentials.util.DateUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,12 +26,18 @@ public class AnimeController {
     private final AnimeService animeService;
 
     @GetMapping
-    public List<Anime> list(){
+    public ResponseEntity<List<Anime>> list(){
         log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
-        return animeService.listAll();
-
+        return new ResponseEntity<>(animeService.listAll(), HttpStatus.OK);
     }
-}
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<Anime> findAnimeById(@PathVariable Long id){
+        return new ResponseEntity<>(animeService.findAnimeById(id), HttpStatus.OK);
+    }
+
+
+}//class
+
 /*
 @RestController -> Retorna um corpo contento somente String/Json
 @Controller -> Retorna uma pagina html inteira
--- src/main/java/br/com/uanderson/springboot2essentials/service/AnimeService.java
+++ src/main/java/br/com/uanderson/springboot2essentials/service/AnimeService.java
@@ -1,18 +1,28 @@
 package br.com.uanderson.springboot2essentials.service;
 
 import br.com.uanderson.springboot2essentials.domain.Anime;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 @Service
 public class AnimeService {
     //-@Service representa uma Class que é reponsável pela implementação da REGRA DE NEGÓCIO da aplicação!
+    private List<Anime> animes = List.of(
+            new Anime(1L,"Naruto"),
+            new Anime(2L,"Boruto"),
+            new Anime(3L,"Boku no Hero Academy")
+    );
 
     public List<Anime> listAll(){
-        return List.of(
-                new Anime(1L,"Naruto"),
-                new Anime(2L,"Boruto"),
-                new Anime(3L,"Boku no Hero Academy")
-        );
+        return animes;
     }
-}
+    public Anime findAnimeById(Long id){
+        return animes.stream()
+                .filter(anime -> anime.getId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, String.format("Anime not found with id '%s'", id)));
+    }
+
+}//class
