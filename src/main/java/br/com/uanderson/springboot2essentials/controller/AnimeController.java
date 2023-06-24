a631a7a46ea825335948bfacc11076851dd19077
Uanderson Ferreira de Oliveira <uanderson.oliveira@estudante.ifto.edu.br>
2023-06-24 18:07:42 -0300

    Método POST save

--- src/main/java/br/com/uanderson/springboot2essentials/controller/AnimeController.java
+++ src/main/java/br/com/uanderson/springboot2essentials/controller/AnimeController.java
@@ -8,10 +8,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -30,10 +27,24 @@ public class AnimeController {
         log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
         return new ResponseEntity<>(animeService.listAll(), HttpStatus.OK);
     }
-    @GetMapping(path = "/{id}")
+    @GetMapping(path = "/{id}")//Quando temos mais de 1 method GET é necessaŕio diferencialos por um 'path' caminho que apronta pra um endpoint /animes/{id}
     public ResponseEntity<Anime> findAnimeById(@PathVariable Long id){
         return new ResponseEntity<>(animeService.findAnimeById(id), HttpStatus.OK);
     }
+
+    @PostMapping
+    //@ResponseStatus(HttpStatus.CREATED) //outra forma de retornar o status
+    public ResponseEntity<Anime> save(@RequestBody Anime anime){
+       /*
+       @RequestBody Anime anime -> Aqui o Jackson entra em cena realizando o mapeamento
+       do objeto recebido no corpo(body) para um "Anime", para isso o nome dos atributos/propriedades
+       devem ser as mesma, caso contrário é necessário informar ao Jackson o nome do atributo que deve ser
+       mapeado, através da anotação:  @JsonProperty("nome do atributo JSON vindo do corpo") em cima do atributo da classe "Anime"
+        */
+        return new ResponseEntity<>(animeService.save(anime), HttpStatus.CREATED);
+
+    }
+
 
 
 }//class
--- src/main/java/br/com/uanderson/springboot2essentials/service/AnimeService.java
+++ src/main/java/br/com/uanderson/springboot2essentials/service/AnimeService.java
@@ -5,24 +5,42 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
 @Service
 public class AnimeService {
     //-@Service representa uma Class que é reponsável pela implementação da REGRA DE NEGÓCIO da aplicação!
-    private List<Anime> animes = List.of(
-            new Anime(1L,"Naruto"),
-            new Anime(2L,"Boruto"),
-            new Anime(3L,"Boku no Hero Academy")
-    );
+
+    private static List<Anime> animes;//Lista imutável, onde todas as instâncias da classe terão acesso
+                                      // à mesma lista, independentemente de quantos objetos sejam criados
+    static {
+        //Bloco static é iniciado primeiro, por isso se cria um intancia de arrayList e atribui a animes
+        animes = new ArrayList<>(//Criar uma instância de arrayList para que seja possível
+                                 // manipular a lista 'animes' que é imutável, senão gera um 'UnsupportedOperationException', pois não aceita modificações
+                List.of(
+                        new Anime(1L,"Naruto"),
+                        new Anime(2L,"Boruto"),
+                        new Anime(3L,"Boku no Hero Academy")
+                )
+        );
+    }
 
     public List<Anime> listAll(){
         return animes;
     }
-    public Anime findAnimeById(Long id){
+    public Anime findAnimeById(long id){
         return animes.stream()
                 .filter(anime -> anime.getId().equals(id))
                 .findFirst()
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, String.format("Anime not found with id '%s'", id)));
     }
 
+    public Anime save(Anime anime) {
+        long id = ThreadLocalRandom.current().nextLong(3, 1000000);
+        anime.setId(id);
+        animes.add(anime);
+        return anime;
+    }
 }//class
