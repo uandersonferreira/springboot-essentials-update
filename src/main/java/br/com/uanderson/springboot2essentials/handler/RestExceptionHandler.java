be453c444431edbf859334f0e8c8ffdda2adcd3d
Uanderson Ferreira de Oliveira <uanderson.oliveira@estudante.ifto.edu.br>
2023-06-27 13:13:31 -0300

    Sobrescrevendo o handler-default dos methods de erros do spring

--- src/main/java/br/com/uanderson/springboot2essentials/handler/RestExceptionHandler.java
+++ src/main/java/br/com/uanderson/springboot2essentials/handler/RestExceptionHandler.java
@@ -2,15 +2,25 @@ package br.com.uanderson.springboot2essentials.handler;
 
 import br.com.uanderson.springboot2essentials.exeption.BadRequestException;
 import br.com.uanderson.springboot2essentials.exeption.BadRequestExceptionDetails;
+import br.com.uanderson.springboot2essentials.exeption.ExceptionDetails;
 import br.com.uanderson.springboot2essentials.exeption.ValidationExceptionDetails;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.log4j.Log4j2;
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.Nullable;
 import org.springframework.validation.FieldError;
+import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.ServletWebRequest;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import org.springframework.web.util.WebUtils;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -18,14 +28,14 @@ import java.util.stream.Collectors;
 
 @ControllerAdvice
 @Log4j2
-public class RestExceptionHandler {
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
    /*
    handler Global -  Manipulação/Tratamento global
 
     */
 
     @ExceptionHandler(BadRequestException.class)
-    public ResponseEntity<BadRequestExceptionDetails> handlerBadRequestException(BadRequestException badRequestException){
+    public ResponseEntity<BadRequestExceptionDetails> handleBadRequestException(BadRequestException badRequestException){
         return new ResponseEntity<>(
           BadRequestExceptionDetails.builder()
                   .title("Bad Request Exception, Check the Documentation")
@@ -49,10 +59,11 @@ public class RestExceptionHandler {
                  */
 
     }
-    @ExceptionHandler(MethodArgumentNotValidException.class)
     @SuppressWarnings("java:S1854")
-    public ResponseEntity<ValidationExceptionDetails> handlerBadRequestException(
-            MethodArgumentNotValidException exception){
+    @Override //Sobrescrevendo um method de tratamento de erro do spring para um personalizado com nossos atributos
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException exception, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+
         log.info("Fields: {}", exception.getBindingResult().getFieldError().getField());//pegar um campo com erro
 
         List<FieldError> fieldErrors = exception.getBindingResult().getFieldErrors();
@@ -74,4 +85,46 @@ public class RestExceptionHandler {
 
     }
 
+    @Override //Sobrescrevendo um method de tratamento de erro do spring para um personalizado com nossos atributos
+    protected ResponseEntity<Object> handleExceptionInternal(Exception ex, @Nullable Object body, HttpHeaders headers,
+                                                             HttpStatusCode statusCode, WebRequest request) {
+
+        ExceptionDetails exceptionDetails = ExceptionDetails.builder()
+                .title(ex.getCause().getMessage())
+                .status(statusCode.value())
+                .details(ex.getMessage())
+                .developerMessage(ex.getClass().getName())
+                .timestamp(LocalDateTime.now())
+                .build();
+
+        return new ResponseEntity<>(exceptionDetails, headers, statusCode);
+
+    }//handleExceptionInternal
+
 }//clas
+/*
+
+OBS: A fim de manter uma padronização das mensagens de erros do spring,
+podemos sobreescrever o comportamento dos method já existente de tratamento
+para um, que tenha como retorno o que queremos. Dentre as classes de erros que podemos
+sobrescrever temos:
+
+	@ExceptionHandler({
+			HttpRequestMethodNotSupportedException.class,
+			HttpMediaTypeNotSupportedException.class,
+			HttpMediaTypeNotAcceptableException.class,
+			MissingPathVariableException.class,
+			MissingServletRequestParameterException.class,
+			MissingServletRequestPartException.class,
+			ServletRequestBindingException.class,
+			MethodArgumentNotValidException.class,
+			NoHandlerFoundException.class,
+			AsyncRequestTimeoutException.class,
+			ErrorResponseException.class,
+			ConversionNotSupportedException.class,
+			TypeMismatchException.class,
+			HttpMessageNotReadableException.class,
+			HttpMessageNotWritableException.class,
+			BindException.class
+		})
+ */
