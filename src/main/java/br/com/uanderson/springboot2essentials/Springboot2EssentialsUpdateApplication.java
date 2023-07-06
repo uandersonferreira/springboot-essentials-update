da27a619b23cdc844111c9642420fa2d1be8d840
Uanderson Ferreira de Oliveira <uanderson.oliveira@estudante.ifto.edu.br>
2023-07-05 21:39:32 -0300

    Monitorando a aplicação com Grafana + Prometheus

--- src/main/java/br/com/uanderson/springboot2essentials/Springboot2EssentialsUpdateApplication.java
+++ src/main/java/br/com/uanderson/springboot2essentials/Springboot2EssentialsUpdateApplication.java
@@ -1,8 +1,11 @@
 package br.com.uanderson.springboot2essentials;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Springboot2EssentialsUpdateApplication {
@@ -11,8 +14,18 @@ public class Springboot2EssentialsUpdateApplication {
 	}
 
 	/*
-	OBSERVATION: Sempre que for criar uma BRANCH e trabalhar nela,
-	fazer o commit das alterações feitas, para que fiquem somente na branch.
+		Configuração de bean para fornecimento das metricas
+		pro grafana através do Dashboard JVM (Micrometer) - 4701
 	 */
+	@Bean
+	MeterRegistryCustomizer<MeterRegistry> configurer(
+			@Value("${spring.application.name}") String applicationName) {
+		return (registry) -> registry.config().commonTags("application", applicationName);
+	}
+
 
 }
+/*
+	OBSERVATION: Sempre que for criar uma BRANCH e trabalhar nela,
+	fazer o commit das alterações feitas, para que fiquem somente na branch.
+*/
